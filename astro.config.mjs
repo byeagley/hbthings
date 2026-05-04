2399f70373002169a7e2f3240c3b1494d53c507b
Bennett Yeagley <46436812+byeagley@users.noreply.github.com>
2026-05-03 22:49:14 -0500

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,51 +1,57 @@
-import { defineConfig, fontProviders } from 'astro/config';
-import { readFileSync, writeFileSync, readdirSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
-import { minify } from 'html-minifier-next';
+import { defineConfig, fontProviders } from "astro/config";
+import { readFileSync, writeFileSync, readdirSync } from "fs";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import { minify } from "html-minifier-next";
 
 // Before `defineConfig`:
 function htmlMinifier() {
   return {
-    name: 'html-minifier-next',
+    name: "html-minifier-next",
     hooks: {
-      'astro:build:done': async ({ dir }) => {
+      "astro:build:done": async ({ dir }) => {
         const distDir = fileURLToPath(dir);
-        const files = readdirSync(distDir, { recursive: true, encoding: 'utf-8' })
-          .filter(f => f.endsWith('.html'))
-          .map(f => join(distDir, f));
+        const files = readdirSync(distDir, {
+          recursive: true,
+          encoding: "utf-8",
+        })
+          .filter((f) => f.endsWith(".html"))
+          .map((f) => join(distDir, f));
         for (const file of files) {
-          const minified = await minify(readFileSync(file, 'utf-8'), {
-            preset: 'comprehensive'
+          const minified = await minify(readFileSync(file, "utf-8"), {
+            preset: "comprehensive",
             // Options: https://github.com/j9t/html-minifier-next?tab=readme-ov-file#options-quick-reference
           });
           writeFileSync(file, minified);
         }
-      }
-    }
+      },
+    },
   };
 }
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://byeagley.github.io',
-    base: '/hbthings',
-    integrations: [htmlMinifier()],
-    fonts: [{
-    provider: fontProviders.local(),
-    name: "Charter",
-    cssVariable: "--font-charter",
-    options: {
-      variants: [{
-        src: ['./src/fonts/charter_regular.woff2'],
-        weight: '500',
-        style: 'normal'
-      }, {
-        src: ['./src/fonts/charter_bold.woff2'],
-        weight: '700',
-        style: 'normal'
-      }]
-    }
-  }, 
-]
+  site: "https://byholdenbrown.com",
+  integrations: [htmlMinifier()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Charter",
+      cssVariable: "--font-charter",
+      options: {
+        variants: [
+          {
+            src: ["./src/fonts/charter_regular.woff2"],
+            weight: "500",
+            style: "normal",
+          },
+          {
+            src: ["./src/fonts/charter_bold.woff2"],
+            weight: "700",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
